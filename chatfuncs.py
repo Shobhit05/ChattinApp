aa7043435a8908d6736e23333bf1fbef022bd63d
Shobhit Srivastava <shobhit.srivastava39@gmail.com>
2016-10-12 11:17:53 +0530

    Update chatfuncs.py

--- chatfuncs.py
+++ chatfuncs.py
@@ -3,35 +3,35 @@ from socket import *
 
 
 	
-def LoadConnectionInfo(chatspace, entrybox):
-    if entrybox != '':
+def connectioninfo(chatspace, EntryText):
+    if EntryText != '':
         chatspace.config(state=NORMAL)
         if chatspace.index('end') != None:
-            chatspace.insert(END, entrybox+'\n')
+            chatspace.insert(END, EntryText+'\n')
             chatspace.config(state=DISABLED)
             chatspace.yview(END)
 
-def LoadMyEntry(chatspace, entrybox):
-    if entrybox != '':
+def myupdate(chatspace, EntryText):
+    if EntryText != '':
         chatspace.config(state=NORMAL)
         if chatspace.index('end') != None:
             LineNumber = float(chatspace.index('end'))-1.0
-            chatspace.insert(END, "You: " + str(entrybox))
+            chatspace.insert(END, "You: " + EntryText)
             chatspace.tag_add("You", LineNumber, LineNumber+0.4)
             chatspace.tag_config("You", foreground="#FF8000", font=("Arial", 12, "bold"))
             chatspace.config(state=DISABLED)
             chatspace.yview(END)
 
 
-def LoadOtherEntry(chatspace, entrybox):
-    if entrybox != '':
+def update2(chatspace, EntryText):
+    if EntryText != '':
         chatspace.config(state=NORMAL)
         if chatspace.index('end') != None:
             try:
                 LineNumber = float(chatspace.index('end'))-1.0
             except:
                 pass
-            chatspace.insert(END, "Other: " + str(entrybox))
+            chatspace.insert(END, "Other: " + EntryText)
             chatspace.tag_add("Other", LineNumber, LineNumber+0.6)
             chatspace.tag_config("Other", foreground="#04B404", font=("Arial", 12, "bold"))
             chatspace.config(state=DISABLED)
